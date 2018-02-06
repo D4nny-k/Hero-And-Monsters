45e41982cea36f489fc4d3fb847adafa004fdd3a
Daniel Khoshnan <dannykhoshnan@yahoo.com>
2018-02-06 13:31:00 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,14 +1,27 @@
+import java.util.*;
 public class Driver{
-	
-	public static void main (String args[]) {
-		
-		System.out.println("BobRoss123");
-		System.out.println("Bobross234");
-		System.out.println("Bobross345");
-		//reee
-		
-		
-	}
-	
-	
+    public static void main(String args[]){
+        Object[][] map = new Object[15][15];
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[i].length; j++){
+                map[i][j] = "[ ? ]";
+            }
+        }
+        printMap(map);
+    }
+    
+    public static void printMap(Object[][] map){
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[i].length; j++){
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void clearMap(){
+        for(int i = 0; i < 10; i++){
+            System.out.println();
+        }
+    }
 }

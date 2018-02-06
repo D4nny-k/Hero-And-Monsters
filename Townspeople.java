57308869669af3745c36e5477ce79ded8616b9bc
Daniel Khoshnan <dannykhoshnan@yahoo.com>
2018-02-06 13:33:36 -0800

    Update and rename Farmer.java to Townspeople.java

---
+++ Townspeople.java
@@ -0,0 +1,27 @@
+public class Townspeople {
+    private int xpos;
+    private int ypos;
+    
+    public Townspeople(){
+        xpos = (int)(Math.random() * 15);
+        ypos = (int)(Math.random() * 15);
+    }
+    
+    public int getX(){
+        return xpos;
+    }
+    
+    public int getY(){
+        return ypos;
+    }
+    
+    public void delete(){
+        xpos = -1;
+        ypos = -1;
+    }
+    
+    public void resetPos(){
+        xpos = (int)(Math.random() * 15);
+        ypos = (int)(Math.random() * 15);
+    }
+}

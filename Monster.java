8fd8ff4b0ba8a794e209ac6b167cb9dd1cc843a0
Daniel Khoshnan <dannykhoshnan@yahoo.com>
2018-02-06 13:30:04 -0800

    Bob ross 123
    bob ross 123

--- Monster.java
+++ Monster.java
@@ -1,85 +1,31 @@
-public class Monster {
-	
+public class Monster implements Enemy{
     private int attack;
     private int health;
     private int speed;
-    private int xpos;
-    private int ypos;
     
-    public Monster() {
-    	
-        attack = (int)(Math.random() * 30 + 1);
-        health = (int)(Math.random() * 100 + 1);
-        speed = (int)(Math.random() * 3);
-        xpos = (int)(Math.random() * 10);
-        ypos = (int)(Math.random() * 10);
-        
+    public Monster(){
+        attack = (int)(Math.random() * 15 + 15);
+        health = (int)(Math.random() * 50 + 50);
+        speed = (int)(Math.random()* 3);
     }
     
-    public int getX() {
-    	
-        return xpos;
-        
-    }
-    
-    public int getY() {
-        
-    	return ypos;
-        
-    }
-    
-    public void setX() {
-    	
-        xpos = (int)(Math.random() * 10);
-        
-    }
-    
-    public void setY() {
-    	
-        ypos = (int)(Math.random() * 10);
-        
-    }
-    
-    public int getSpeed() {
-        
-    	return speed;
-    	
-    }
-    
-    public int getAttack() {
-    	
+    public int getAttack(){
         return attack;
-        
     }
     
-    public void updateHealth(int value) {
-    	
-        health += value;
-    
-    }
-    
-    public int getHealth() {
-    	
+    public int getHealth(){
         return health;
-        
     }
     
-    public void setHealth(int value) {
-    	
-        health = value;
-        
+    public int getSpeed(){
+        return speed;
     }
     
-    public void delete() {
-    	
-        xpos = -10;
-        ypos = -10;
-        
+    public void updateHealth(int value){
+        health -= value;
     }
     
-    public String toString() {
-    	
-        return "M";
-        
+    public void resetHealth(){
+        health = 0;
     }
 }

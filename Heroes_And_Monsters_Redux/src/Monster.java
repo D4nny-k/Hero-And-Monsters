a3db8236fc39a162c81b7d2e52abe5ea1ad5c9fa
Daniel Khoshnan <dannykhoshnan@yahoo.com>
2018-03-06 09:13:03 -0800

    Update Monster.java

--- Heroes_And_Monsters_Redux/src/Monster.java
+++ Heroes_And_Monsters_Redux/src/Monster.java
@@ -2,11 +2,13 @@ public class Monster implements Enemy{
     private int attack;
     private int health;
     private int speed;
+    private String direction;
     
-    public Monster(){
+    public Monster(String direction){
         attack = (int)(Math.random() * 25 + 5);
         health = (int)(Math.random() * 80 + 20);
         speed = (int)(Math.random()* 3);
+        this.direction = direction;
     }
     
     public int getAttack(){
@@ -19,6 +21,10 @@ public class Monster implements Enemy{
     
     public int getSpeed(){
         return speed;
+    }
+    
+    public String getDirection(){
+        return direction;
     }
     
     public void updateHealth(int value){
